f2cd68b6dfb7b7dbeff431cd39e6f34ee4c3df2a
deva567 <40196767+deva567@users.noreply.github.com>
2020-06-24 22:43:11 +0530

    Update DataFrameToDict.py

--- vennam/DataFrameToDict.py
+++ vennam/DataFrameToDict.py
@@ -2,10 +2,10 @@ def dftodict(dataframe):
     """This function takes the dataframe
     as a parameter and convert to dictionary """
 
-    dummy_dict={key:"" for key in dataframe.columns.to_list()}
-    dummy_dict_lst=[]
+    tmp_dict={key:"" for key in dataframe.columns.to_list()}
+    tmp_dict_lst=[]
     for index,row in dataframe.iterrows():
-        dummy_dict1={key:row[key] for key in dummy_dict.keys()}
-        dummy_dict_lst.append(dummy_dict1.copy())
-    return dummy_dict_lst
+        tmp_dict1={key:row[key] for key in tmp_dict.keys()}
+        tmp_dict_lst.append(tmp_dict1.copy())
+    return tmp_dict_lst
   
